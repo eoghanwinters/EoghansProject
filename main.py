874cf5100b66593c51e5604fab287e4a405e6357
eoghanwinters <eoghanwinters1@hotmail.co.uk>
2020-03-05 13:23:58 +0000

    CSS added

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+	{
+		'author': 'Eoghan Winters',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': '5th March 2020'
+	},
+	{
+		'author': 'Ruairi Winters',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': '6th March 2020'
+	}
+]
+
+
 @app.route("/")
+@app.route("/home")
 def home():
-	return render_template("home.html")
+	return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-	return render_template("about.html")
+	return render_template('about.html', title='About')
 
 if __name__ == "__main__":
 	app.run(debug=True, host="0.0.0.0")
