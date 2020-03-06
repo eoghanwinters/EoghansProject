b382decea4fb555ab72ec25f43c2a595f34fdfde
eoghanwinters <eoghanwinters1@hotmail.co.uk>
2020-03-06 18:29:53 +0000

    Forms added

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'e20fe483cc596f15763b9fdca9b2e841'
+
 
 posts = [
 	{
@@ -25,6 +29,25 @@ def home():
 @app.route("/about")
 def about():
 	return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account created for {form.username.data}!', 'success')
+		return redirect(url_for('home'))
+	return render_template('register.html', title='register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		if form.email.data == 'admin@admin.com' and form.password.data == 'password':
+			flash('You have been logged in!', 'success')
+			return redirect(url_for('home'))
+		else:
+			flash('Login Unsuccessful', 'danger')
+	return render_template('login.html', title='login', form=form)
 
 if __name__ == "__main__":
 	app.run(debug=True, host="0.0.0.0")
